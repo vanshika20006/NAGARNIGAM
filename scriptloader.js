edeedfc2b3ede1ae246c8f44eb09c207cb61243f
Dravya7 <dravyashah25@gmail.com>
2024-06-27 14:25:52 +0530

    Add files via upload

---
+++ scriptloader.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.getElementById('loader');
+  
+    loader.style.display = 'block';
+  
+    window.addEventListener('load', function() {
+      loader.style.display = 'none';
+    });
+  
+  });
+  
